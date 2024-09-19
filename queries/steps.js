ba51c6c4f13e224958727da52ed5c7106fbcbce6
llamouth <150949923+llamouth@users.noreply.github.com>
2024-09-19 08:23:33 -0400

    update: update steps queries

--- queries/steps.js
+++ queries/steps.js
@@ -4,25 +4,30 @@ const db = require ('../db/dbConfig')
 const getAllSteps = async(user_id) => {
     try {
         const allSteps = await db.any('SELECT * FROM steps WHERE user_id=$1', user_id)
+        return allSteps
     } catch (error) {
         error
     }
 }
 
 
-const getSteps = async (user_id, id) => {
+const getSingleStep = async (user_id, id) => {
     try {
-        const oneSteps = await db.one('SELECT * FROM steps WHERE id=$1 AND user_id=$2', [id, user_id])
+        const oneStep = await db.one('SELECT * FROM steps WHERE id=$1 AND user_id=$2', [id, user_id])
+        return oneStep
     } catch (error) {
         error
     }
 }
 
 
-const deleteSteps = async (user_id,id) => {
+const deleteSteps = async (id) => {
     try {
-        const deletedSteps = await db.one('DELETE * FROM steps WHERE id=$1', [id,user_id])
+        const deletedSteps = await db.one('DELETE * FROM steps WHERE id=$1', [id])
+        return deletedSteps
     } catch (error) {
         
     }
 }
+
+module.exports = {getAllSteps, getSingleStep, deleteSteps }
